40469b9129e0891c6c559c885b6acb396653cb01
S006449 <Wuhaiword@123456>
2020-12-04 10:43:48 +0800

    初始化

---
+++ eureka-server01/src/main/java/com/yhd/eurekaserver01/EurekaServer01Application.java
@@ -0,0 +1,18 @@
+package com.yhd.eurekaserver01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 11月份
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer01Application.class, args);
+    }
+
+}
---
+++ eureka-server01/src/test/java/com/yhd/eurekaserver01/EurekaServer01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhd.eurekaserver01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
