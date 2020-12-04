6a21e2ed9db623b855b9d84cf2687d0e49763dcb
S005887 <644258812@qq.com>
2020-12-04 16:37:59 +0800

    算法多少的

--- eureka-server01/src/main/java/com/yhd/eurekaserver01/EurekaServer01Application.java
+++ eureka-server01/src/main/java/com/yhd/eurekaserver01/EurekaServer01Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * 11月份
+ *
  */
 @SpringBootApplication
 @EnableEurekaServer
